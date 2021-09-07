b10315904087b8319ecb531d488cb2ef02799cb7
Francisco Baptista <francisco.baptista@outsystems.com>
2021-09-07 11:40:21 +0100

    Program implementation

---
+++ FetchRequests/Logger.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Reflection;
+
+namespace FetchRequests {
+    class Logger {
+
+        private static string _consoleLog = Path.Combine(Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location), "ConsoleLog.txt");
+
+        public static void TraceLog(string message) {
+
+            File.AppendAllText(_consoleLog, DateTime.Now + "\t" + "[INFO] " + message + Environment.NewLine);
+
+        }
+
+    }
+}
--- FetchRequests/Program.cs
+++ FetchRequests/Program.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
+using System.Threading;
 using Microsoft.Web.Administration;
 using System.IO;
 namespace FetchRequests {
@@ -10,26 +10,31 @@ namespace FetchRequests {
 
         private static string _currDir = AppDomain.CurrentDomain.BaseDirectory;
 
+        // needs testing
+
         static void Main(string[] args) {
 
-            // args[0] Request URL to catch. Multiple URLs must be separated by pipe |
+            // args[0] Request URL to catch. Multiple URLs must be separated by pipe | "All" to monitor all requests
             // args[1] Execution time threshold in seconds - integer
             // args[2] Rounds to execute - integer
+            // args[3] Sleep time between checks in seconds - integer
 
-            string urls;
-            int execTime, rounds;
+            string rawUrls;
+            int execTime, rounds, sleepTime, currentRound;
 
             if (args.Length.Equals(0) || args[1].Length.Equals(0)) {
 
                 // implement help section with examples
                 Console.WriteLine("Invalid arguments. Run FetchRequests.exe help for more information or read the README");
+                Console.ReadLine();
+                return;
 
             } else {
 
                 // input validations
                 try {
 
-                    urls = args[0].Trim().ToString();
+                    rawUrls = args[0].Trim().ToString();
                     execTime = Convert.ToInt32(args[1]);
 
                     if (args[2].Length.Equals(0)) {
@@ -39,6 +44,16 @@ namespace FetchRequests {
                     } else {
 
                         rounds = Convert.ToInt32(args[2]);
+                    }
+
+                    if (args[3].Length.Equals(0)) {
+
+                        sleepTime = 60; // default sleep time - 60 seconds
+
+                    } else {
+
+                        sleepTime = Convert.ToInt32(args[3]);
+
                     }
 
                 } catch (Exception e) {
@@ -51,34 +66,61 @@ namespace FetchRequests {
 
             }
 
-            // implement program
+
+            // inputs processing
+            List<string> parsedUrls = new List<string>();
+
+            parsedUrls = rawUrls.ToLower().Split('|').ToList();
+
+            Logger.TraceLog("Urls: " + rawUrls + "; " + "Execution time thresold: " + execTime + "; " + "Rounds: " + rounds + "; " + "Sleep time: " + sleepTime + ";");
+
+
+            // program
                 using (ServerManager manager = new ServerManager()) {
 
-                foreach (WorkerProcess proc in manager.WorkerProcesses) {
+                currentRound = 0;
+                while(currentRound < rounds) {
 
-                    if (proc.AppPoolName.Equals("ServiceCenterAppPool")) {
-
+                    foreach (WorkerProcess proc in manager.WorkerProcesses) {
 
                         List<string> requests = new List<string>();
                         RequestCollection rc;
 
-                        rc = proc.GetRequests(1000);
+                        rc = proc.GetRequests(execTime);
 
-                        foreach(var request in rc){
+                        foreach (string url in parsedUrls) {
 
-                            if (request.Url.ToLower().Contains("/servicecenter")) {
+                            foreach (var request in rc) {
 
-                                CmdHelper.RunCommand(Path.Combine(_currDir, @"OSDiagTool\OSDiagTool.exe RunCmdLine")); // Get thread dumps; Configuration file must be set for thread dumps only
+                                if (request.Url.ToLower().Contains(url)) {
+
+                                    Logger.TraceLog("Caught request: " + request.Url + "; " + "Execution time elapsed: " + request.TimeElapsed);
+                                    Logger.TraceLog("Starting to catch thread dumps");
+                                    Console.WriteLine("Starting to catch thread dumps");
+
+                                    CmdHelper.RunCommand(Path.Combine(_currDir, @"OSDiagTool\OSDiagTool.exe RunCmdLine")); // Get thread dumps; Configuration file must be set for thread dumps only
+
+                                    Logger.TraceLog("Finished catching thread dumps");
+                                    Console.WriteLine("Finished catching thread dumps");
+
+                                    currentRound++;
+                                    Thread.Sleep(sleepTime);
+
+                                }
+
+                                break;
 
                             }
 
-                        }                    
+                            break;
 
-                        Console.ReadLine();
-
-
+                        }
                     }
                 }
+
+                Logger.TraceLog("Exiting console app");
+                Console.WriteLine("Exiting console app");
+
             }
         }
     }
